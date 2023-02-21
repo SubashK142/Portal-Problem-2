7285037c0b3f94a024773debbc54e996f3087f03
Subashk1432 <727721euai055@skcet.ac.in>
2023-02-21 17:52:27 +0530

    FIRST

---
+++ src/main/java/com/example/demo/Webframeworkproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webframeworkproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webframeworkproject1Application.class, args);
+	}
+
+}
